9facd65a94722f6ed5648ccfa7afa8827caa1650
Chukwukelunze Chiagozie Melvin <odogwuml01@gmail.com>
2023-08-20 02:48:00 +0100

    Refactor: removed whitespaces

--- dbfunctions.py
+++ dbfunctions.py
@@ -25,27 +25,18 @@ def add_product():
         return "\nproduct has been added successfully"
 
 def update_product():
-
     # get the product id
     product_id = input("\nEnter product id: ")
-
     # select desired row
     cdb.cursor.execute("SELECT * FROM products WHERE id=?", [product_id])
-
     row = cdb.cursor.fetchone()
-
     if row:
         print("\nHere are the products details: \n")
         print(f"id: {row[0]} \nname: {row[1]} \ncategory: {row[2]} \nprice: {row[3]} \nquantity: {row[4]}")
-
         should_continue = True
-
         while should_continue:
-
             print("\nWhere would you like to make channges to: \n\nname \nid \ncategory \nprice or \nquantity\n")
-
             required_change = input("")
-
             if required_change == "name":
                 change = input(f"\nEnter new product name: ")
             elif required_change == "id":
@@ -59,46 +50,34 @@ def update_product():
             else:
                 print("Invalid Entry")
                 continue
-
             # make required change to the product
             cdb.cursor.execute(f"UPDATE products SET {required_change}=? WHERE id=?",(change, product_id))
             cdb.conn.commit()
             print("\nProduct updated successfully")
-
             # Fetch and display the updated row
             cdb.cursor.execute("SELECT * FROM products WHERE id=?", [product_id])
             updated_row = cdb.cursor.fetchone()
             print("\nUpdated information:")
             print(f"id: {updated_row[0]} \nname: {updated_row[1]} \ncategory: {updated_row[2]} \nprice: {updated_row[3]} \nquantity: {updated_row[4]}")
-
             if input("Do you still want to make changes to this product yes/no: ").lower() == "no":
                 should_continue = False
     else:
-        return "\nNo row found with the specified ID."
-    
+        return "\nNo row found with the specified ID."    
     return "\n"
 
-
 def search():
-
     should_continue = True
-
     while should_continue:
         # get desired product id
         product_id = input("Enter products id: ")
-
         # select desired row
         cdb.cursor.execute("SELECT * FROM products WHERE id=?", [product_id])
-
         row = cdb.cursor.fetchone()
-
         if row:
             print("\nHere are the product details: \n")
             print(f"id: {row[0]} \nname: {row[1]} \ncategory: {row[2]} \nprice: {row[3]} \nquantity: {row[4]}")
-
             if input("\nDo you want to search for another row? yes/no: ") == "no":
                 should_continue = False
         else:
             return "\nNo row found with the specified ID."
-
     return
